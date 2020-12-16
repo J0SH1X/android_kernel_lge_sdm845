4970e45d01843eb79a13f676f4fac28a15c108a1
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-16 12:13:08 +0100

    sdm845: spoof safetynet props

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -1,3 +1,5 @@
+
+
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/proc_fs.h>
@@ -5,7 +7,8 @@
 #include <asm/setup.h>
 #include <soc/qcom/lge/board_lge.h>
 #include <linux/slab.h>
-	
+#include <asm/setup.h>
+
 static char updated_command_line[COMMAND_LINE_SIZE];
 	
 static void proc_cmdline_set(char *name, char *value)	
@@ -50,12 +53,41 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
+static void patch_flag(char *cmd, const char *flag, const char *val)
+{
+	size_t flag_len, val_len;
+	char *start, *end;
+
+	start = strstr(cmd, flag);
+	if (!start)
+		return;
+
+	flag_len = strlen(flag);
+	val_len = strlen(val);
+	end = start + flag_len + strcspn(start + flag_len, " ");
+	memmove(start + flag_len + val_len, end, strlen(end) + 1);
+	memcpy(start + flag_len, val, val_len);
+}
+
+static void patch_safetynet_flags(char *cmd)
+{
+	patch_flag(cmd, "androidboot.flash.locked=", "1");
+	patch_flag(cmd, "androidboot.verifiedbootstate=", "green");
+	patch_flag(cmd, "androidboot.veritymode=", "enforcing");
+	patch_flag(cmd, "androidboot.vbmeta.device_state=", "locked");
+}
+
 static int __init proc_cmdline_init(void)
 {
 	// copy it only once	
 	strcpy(updated_command_line, saved_command_line);
 
+        // Patch Safetynet flags
+        patch_safetynet_flags(updated_command_line);
+
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
 fs_initcall(proc_cmdline_init);
+
+
